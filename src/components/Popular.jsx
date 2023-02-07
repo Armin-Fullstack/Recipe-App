5b24f9f387d5d763e713a7b86ad4c7f95dfa9c52
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-07 23:54:30 +0330

    Implemented the getPopular function

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react"
+
 
 const Popular = () => {
+
+    useEffect(() => {
+        getPopular()
+    },[])
+
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apikey=${process.env.REACT_KEY_API_KEY}&number=9`)
+        const data = await api.json()
+        console.log(data)
+    }
+
+
+
+
     return(
+        git
         null
     )
 }
