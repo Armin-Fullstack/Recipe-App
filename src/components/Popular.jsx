a28f960ac8fb5d58e902b0cdc27fb120de12666d
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-09 00:04:15 +0330

    I saved the data in a state

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect , useState } from "react";
 
 
 
 const Popular = () => {
+    const [popular , setPopular] = useState([])
 
     useEffect(() => {
 
@@ -14,7 +15,7 @@ const Popular = () => {
     const getPopular = async () => {
        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_APP_API_KEY}&number=9`)
        const data = await api.json()
-       console.log(data)
+       setPopular(data.recipes)
     } 
 
     return(
