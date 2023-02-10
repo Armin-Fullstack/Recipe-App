80783ffe57c64040f3c0449d84b2a6dbc0aaa10b
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-10 17:25:03 +0330

    Rendering the cards

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,26 +1,46 @@
-import { useEffect , useState } from "react";
-
-
+import { useEffect, useState } from "react";
 
 const Popular = () => {
-    const [popular , setPopular] = useState([])
+  const [popular, setPopular] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    getPopular();
+  }, []);
 
-        getPopular()
-       
-    } , [])
+// fetch data
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${
+        import.meta.env.VITE_APP_API_KEY
+      }&number=9`
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+  };
 
-    
-    const getPopular = async () => {
-       const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_APP_API_KEY}&number=9`)
-       const data = await api.json()
-       setPopular(data.recipes)
-    } 
+  return (
+    <>
+        {/* section container */}
+      <div className="my-[4rem]">
+        <h1>Popular Picks</h1>
 
-    return(
-       null
-    )
-}
+        {/* rendering cards */}
+        {popular.map((element) => {
+          return (
+            // card container
+            <div className="min-h-[25rem] rounded-[2rem] overflow-hidden">
+              <p>{element.title}</p>
+              <img
+                src={element.image}
+                alt={element.title}
+                className="rounded-[2rem]"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Popular;
