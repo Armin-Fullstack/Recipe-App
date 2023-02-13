d19908601f209f442eefa134f4b93348a53696f3
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-13 20:11:27 +0330

    Feat: Added the cuisine container

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,31 +1,32 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Cuisine = () => {
-    const [cuisine , setCuisine] = useState([])
-    let params = useParams()
-    //Fetching api
-    const getCuisine = async (type) => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${
-            import.meta.env.VITE_APP_API_KEY
-          }&cuisine=${type}`)
-          const data = await api.json()
-          setCuisine(data.results)
-    }
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+  //Fetching api
+  const getCuisine = async (type) => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
+        import.meta.env.VITE_APP_API_KEY
+      }&cuisine=${type}`
+    );
+    const data = await api.json();
+    setCuisine(data.results);
+  };
 
-    // Invoking the getCuisine function
-    useEffect(() => { 
-        getCuisine(params.type)
-    } , [params.type])
+  // Invoking the getCuisine function
+  useEffect(() => {
+    getCuisine(params.type);
+  }, [params.type]);
 
-    return (
-
-        <>
-
-        null
+  return (
+        // cuisine container 
+    <div className="max-w-5xl mx-auto px-10 grid gap-4 grid-cols-4">
         
-        </>
-    )
-}
+    </div>
+      
+  );
+};
 
-export default Cuisine
+export default Cuisine;
