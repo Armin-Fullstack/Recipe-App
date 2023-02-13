13ae3108678e0b82c6d4e98f0934cf7f63f295d1
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-13 18:41:21 +0330

    Feat: Fetched the data from the API

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
 
 const Cuisine = () => {
+    const [cuisine , setCuisine] = useState([])
+    //Fetch api
+    const getCuisine = async (type) => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${
+            import.meta.env.VITE_APP_API_KEY
+          }&cuisine=${type}`)
+          const data = await api.json()
+          setCuisine(data.results)
+    }
+
     return (
-        <h1>Cuisine</h1>
+
+        <>
+
+        null
+        
+        </>
     )
 }
 
