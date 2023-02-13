bbf319e58f4ba4ae23ed87d547805e176d839563
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-13 17:51:03 +0330

    Fix: Fixed the BrowserRouter issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
 
 const App = () => {
   return(
     <>
-    
+    <BrowserRouter>
     <Category/>
     <Pages/>
+    </BrowserRouter>
+    
     
     </>
     
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,18 +1,18 @@
 import Cuisine from "./Cuisine";
 import Home from "./Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 
 const Pages = () => {
   return (
     <>
-      <BrowserRouter>
+      
         <Routes>
           {/* If the path matches / then render out Home component */}
           <Route path="/" element={<Home />} />
           {/* If the path matches /cuisine then render out Home component */}
           <Route path="/cuisine" element={<Cuisine />} />
         </Routes>
-      </BrowserRouter>
+      
     </>
   );
 };
