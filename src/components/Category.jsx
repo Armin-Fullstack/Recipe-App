75d127ef770568410fd2f1bdac6c62be0d7094b7
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-13 17:31:22 +0330

    Feat: Imported the NavLink

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,5 +1,6 @@
 import {FaPizzaSlice , FaHamburger} from "react-icons/fa"
 import {GiNoodles , GiChopsticks} from "react-icons/gi"
+import { NavLink } from "react-router-dom"
 
 const Category = () => {
     return (
