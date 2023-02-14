520f18539fbb0f819eaef67421cd942f40d14a77
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-14 22:29:38 +0330

    Feat: Added the navlink class

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -8,22 +8,22 @@ const Category = () => {
         {/* Icons container */}
         <div className="flex justify-center items-center my-[2rem]">
                 {/* Icons 1 */}
-                <NavLink to={"/cuisine/Italian"}>
+                <NavLink to={"/cuisine/Italian"} className="navlink">
                     <FaPizzaSlice/>
                     <h4>Italian</h4>
                 </NavLink>
                 {/* Icons 2 */}
-                <NavLink to={"/cuisine/American"}>
+                <NavLink to={"/cuisine/American"} className="navlink">
                     <FaHamburger/>
                     <h4>American</h4>
                 </NavLink>
                 {/* Icons 3 */}
-                <NavLink to={"/cuisine/Thai"}>
+                <NavLink to={"/cuisine/Thai"} className="navlink">
                     <GiNoodles/>
                     <h4>Thai</h4>
                 </NavLink>
                 {/* Icons 4 */}
-                <NavLink to={"/cuisine/Japanese"}>
+                <NavLink to={"/cuisine/Japanese"} className="navlink">
                     <GiChopsticks/>
                     <h4>Japanese</h4>
                 </NavLink>
