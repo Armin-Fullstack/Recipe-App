add0b5ae5da396f42d46befec239d11706a8d4e8
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-15 15:40:50 +0330

    Feat: Added a state for input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,14 @@
 import {FaSearch} from "react-icons/fa"
 
 const Search = () => {
+    const [input , setInput] = useState("")
     return(
         <>
             <form action="#" className="mx-[20rem]">
                 <div className="relative">
                     <FaSearch className="absolute top-1/2 left-0 text-white translate-x-full
                      -translate-y-1/2"/>
-                    <input type="text" className="form-input"/>
+                    <input type="text" value={input} className="form-input"/>
                 </div>
             </form>
         </>
