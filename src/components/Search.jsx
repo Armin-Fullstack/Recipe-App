cf9b9813d6b89e94582c45c014dda1a6e9ede444
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-15 15:47:59 +0330

    Feat: Added the getUserInput function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,21 @@
 import {FaSearch} from "react-icons/fa"
+import {useState} from "react"
 
 const Search = () => {
     const [input , setInput] = useState("")
+
+    const getUserInput = (e) => {
+        setInput(e.target.value)
+    }
+
     return(
         <>
             <form action="#" className="mx-[20rem]">
                 <div className="relative">
                     <FaSearch className="absolute top-1/2 left-0 text-white translate-x-full
                      -translate-y-1/2"/>
-                    <input type="text" value={input} className="form-input"/>
+                    <input onChange={getUserInput} type="text" value={input} className="form-input"/>
+                    
                 </div>
             </form>
         </>
