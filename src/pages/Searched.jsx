f61a89c64d08dad6f76107b8d44ad07d9009a18a
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-15 17:33:41 +0330

    Feat: Fetched the searched data

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,8 +1,16 @@
-
 const Searched = () => {
-    return (
-        <h1>Searched</h1>
-    )
-}
+  //Fetch data
+  const getSearched = async (name) => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
+        import.meta.env.VITE_APP_API_KEY
+      }&query=${name}`
+    );
+
+    const data = await api.json();
+  };
+
+  return <h1>Searched</h1>;
+};
 
 export default Searched;
