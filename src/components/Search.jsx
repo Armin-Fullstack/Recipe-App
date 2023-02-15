8acf16957fdf6544865d43567c0342243511072d
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-15 17:25:57 +0330

    Feat: Created the onSubmitHandler function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,25 @@
 import {FaSearch} from "react-icons/fa"
 import {useState} from "react"
+import {useNavigate} from "react-router-dom"
 
 const Search = () => {
     const [input , setInput] = useState("")
+    const navigate = useNavigate()
 
     const getUserInput = (e) => {
         setInput(e.target.value)
+        
     }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault()
+        navigate(`/searched/${input}`)
+    }
+
 
     return(
         <>
-            <form action="#" className="mx-[20rem]">
+            <form onSubmit={onSubmitHandler} className="mx-[20rem]">
                 <div className="relative">
                     <FaSearch className="absolute top-1/2 left-0 text-white translate-x-full
                      -translate-y-1/2"/>
