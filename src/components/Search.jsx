a7d78c2e14f176724ceffe7c9eb29452a85da038
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-15 15:33:54 +0330

    Feat: Implemented the search box

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,16 @@
+import {FaSearch} from "react-icons/fa"
 
 const Search = () => {
     return(
-        null
+        <>
+            <form action="#" className="mx-[20rem]">
+                <div className="relative">
+                    <FaSearch className="absolute top-1/2 left-0 text-white translate-x-full
+                     -translate-y-1/2"/>
+                    <input type="text" className="form-input"/>
+                </div>
+            </form>
+        </>
     )
 }
 
