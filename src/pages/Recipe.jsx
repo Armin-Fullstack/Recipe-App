01d1dab6146ef2c9368f8d7c5a13d82a001c625f
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-17 19:31:30 +0330

    Feat: fetched the details

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,5 +1,20 @@
+import { useParams } from "react-router-dom"
+
 
 const Recipe = () => {
+    
+    let params = useParams()
+
+    // fetch details 
+    const getDetails = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${
+            import.meta.env.VITE_APP_API_KEY
+          }`)
+          const data = api.json()
+          
+    }
+
+
     return(
         null
     )
