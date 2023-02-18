75c9860ba96f3aa00c8460fd0a267eac9b8c11b8
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-18 16:16:27 +0330

    Feat: Implemented the instructions

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 const Recipe = () => {
   const [details, setDetails] = useState({});
-  const [activeTab , setActiveTab] = useState("Instructions")
+  const [activeTab, setActiveTab] = useState("Instructions");
   let params = useParams();
 
   // fetch details
@@ -28,21 +28,48 @@ const Recipe = () => {
       {/* main container  */}
       <div className="container max-w-5xl mx-auto">
         {/* Details container  */}
-        <div className="flex justify-around">
+        <div className="flex justify-evenly">
           {/* title and image container  */}
           <div className="flex flex-col space-y-3">
             <h2>{details.title}</h2>
-            <img src={details.image} alt={details.title} />
+            <img src={details.image} alt={details.title} className="w-80" />
           </div>
 
           {/* info container  */}
           <div className="flex flex-col space-y-4 items-start">
             {/* button container  */}
             <div className="flex justify-center items-center space-x-4">
-              <button onClick={() => setActiveTab("Instructions")} className={`${activeTab === "Instructions" ? "active-btn" : ""} px-5 py-2 border-2 border-black`}>Instructions</button>
+              <button
+                onClick={() => setActiveTab("Instructions")}
+                className={`${
+                  activeTab === "Instructions" ? "active-btn" : ""
+                } px-5 py-2 border-2 border-black`}
+              >
+                Instructions
+              </button>
 
-              <button onClick={() => setActiveTab("Ingredients")} className={`${activeTab === "Ingredients" ? "active-btn" : ""} px-5 py-2 border-2 border-black`}>Ingredients</button>
+              <button
+                onClick={() => setActiveTab("Ingredients")}
+                className={`${
+                  activeTab === "Ingredients" ? "active-btn" : ""
+                } px-5 py-2 border-2 border-black`}
+              >
+                Ingredients
+              </button>
             </div>
+            {activeTab === "Instructions" && (
+              <>
+                <p
+                  dangerouslySetInnerHTML={{ __html: details.summary }}
+                  className="max-w-md font-semibold"
+                ></p>
+                <p
+                  dangerouslySetInnerHTML={{ __html: details.instructions }}
+                  className="max-w-md font-semibold"
+                ></p>
+              </>
+            )}
+
           </div>
         </div>
       </div>
