de504af72f4ca4280be965d8e6f0458698eca619
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-19 18:15:41 +0330

    Feat: Responsive- Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -18,16 +18,16 @@ const Search = () => {
 
 
     return(
-        <>
-            <form onSubmit={onSubmitHandler} className="mx-[20rem]">
+        <div className="main-container px-6 md:px-0">
+            <form onSubmit={onSubmitHandler} className="max-w-xl mx-auto">
                 <div className="relative">
                     <FaSearch className="absolute top-1/2 left-0 text-white translate-x-full
-                     -translate-y-1/2"/>
+                     -translate-y-1/2 cursor-pointer"/>
                     <input onChange={getUserInput} type="text" value={input} className="form-input"/>
                     
                 </div>
             </form>
-        </>
+        </div>
     )
 }
 
